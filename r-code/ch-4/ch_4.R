a38f5e874439d8fe62733fed6fd90ec6ce13a975
Statistical Grimoire <statistical-grimoire@proton.me>
2025-06-03 21:52:31 -0600

    Ch. 4 edits

--- r-code/ch-4/ch_4.R
+++ r-code/ch-4/ch_4.R
@@ -17,6 +17,10 @@ filter(skulls, table %in% c(1, 2)) |>
 library(tidyverse)
 skulls <- read_csv("https://raw.githubusercontent.com/statistical-grimoire/book/refs/heads/main/data/Egyptian-skulls/Thomson_Randall-MacIver_1905.csv")
 
+ncol(skulls) # Number of columns
+colSums(!is.na(skulls)) # Number of rows, excluding NA values
+sum(colSums(!is.na(skulls))) # Total number of values
+
 skulls <- skulls |> 
   select(table:sex, cc) |> # grab relevant columns
   drop_na(cc) # remove rows containing NAs
